badc2e1b0853bab2fde95dcb440767de4e727904
Garth N. Wells <gnw20@cam.ac.uk>
2020-03-25 18:27:49 +0000

    Fix print statement.

--- ffcx/codegeneration/jit.py
+++ ffcx/codegeneration/jit.py
@@ -78,7 +78,6 @@ def get_cached_module(module_name, object_names, cache_dir, timeout):
                 spec.loader.exec_module(compiled_module)
 
                 compiled_objects = [getattr(compiled_module.lib, "create_" + name)() for name in object_names]
-                print("NNNN Not from cache:", cache_dir)
                 return compiled_objects, compiled_module
 
             logger.info("Waiting for {} to appear.".format(str(ready_name)))
